8b082f34e921b84e7de4332da83dfd5079e02e0c
Tomas Tomecek <ttomecek@redhat.com>
2016-02-05 12:22:49 +0100

    correct signal name in print

--- stopsignal/signal_printer.py
+++ stopsignal/signal_printer.py
@@ -6,7 +6,7 @@ import signal
 SIGNALS_TO_NAMES_DICT = dict((getattr(signal, n), n) \
     for n in dir(signal) if n.startswith('SIG') and '_' not in n )
 
-print("My PID is %d, you can stop me with SIGINT1" % os.getpid())
+print("My PID is %d, you can stop me with SIGUSR1" % os.getpid())
 
 while True:
     info = signal.sigwaitinfo(range(1, signal.NSIG))
